1c38f6b85aba838c8c53b2000e583bf87a1307f1
Aaron Wagner <awagner@adherecreative.com>
2015-05-08 16:19:40 -0500

    Some off-canvas adjustments.

--- files/layout/js/app.js
+++ files/layout/js/app.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
 // ====================================
 // Off Canvas Nav
 // ====================================
+	//Copy the nav to the off canvas container
+	$('.off-canvas-wrapper').append($('header nav').html());
+	$('.off-canvas-wrapper>ul>li.hs-item-has-children').append('<span class="expand">+</span>');
+	$('.expand').click(function(e){
+		e.preventDefault();
+		var submenu = $(this).parent().find('ul');
+		submenu.toggle(300);
+		$(this).toggleText('+','\u2013');
+	});
 	function toggleOffCanvas(){
 		$('.off-canvas,.off-canvas-overlay,body').toggleClass('off-canvas-expanded');
 	}
@@ -10,3 +19,8 @@ $(document).ready(function(){
 		toggleOffCanvas();
 	});
 });
+$.fn.toggleText = function(t1, t2){
+  if (this.text() === t1) {this.text(t2);}
+  else                    {this.text(t1);}
+  return this;
+};
